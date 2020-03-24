bd897da5dc5e1a6342daec65ad26fe19ceb3e1f8
Бакайкин Егор <georgiy.bakaykin@gazprombank.ru>
2020-03-24 15:37:48 +0300

    Initial Commit

---
+++ Product Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Product Test
+//
+//  Created by George Bakaykin on 24.03.20.
+//  Copyright © 2020 George Bakaykin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
